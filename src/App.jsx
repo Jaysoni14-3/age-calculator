3982b7234dda32cee8c63529b75734c57ba3558b
Jaysoni14-3 <jaysoni998.js@gmail.com>
2023-08-01 03:06:47 +0530

    Routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter , Routes, Route } from 'react-router-dom'
 import AgeCalculator from './Pages/AgeCalculator'
 import BmiCalculator from './Pages/BmiCalculator'
 import Home from './Pages/Home'
@@ -8,13 +8,13 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter >
         <Routes>
           <Route index path='/' element={<Home />} />
           <Route path='/age-calculator' element={<AgeCalculator />} />
           <Route path='/bmi-calculator' element={<BmiCalculator />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
